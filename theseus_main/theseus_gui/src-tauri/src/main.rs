798aea97f873218a62ac547eb562fb17e9cb0bcf
Isaac Kletzli <isaackletzli@gmail.com>
2024-04-10 11:18:32 -0600

    Fixed main function so it would not hang forever

--- theseus_main/theseus_gui/src-tauri/src/main.rs
+++ theseus_main/theseus_gui/src-tauri/src/main.rs
@@ -14,17 +14,6 @@ mod error;
 #[cfg(target_os = "macos")]
 mod macos;
 
-// Should be called in launcher initialization
-// #[tracing::instrument(skip_all)]
-// async fn initialize_state(app: tauri::AppHandle) -> api::Result<()> {
-//     theseus::EventState::init(app).await?;
-//     let s = State::get().await?;
-//     State::update();
-
-//     s.children.write().await.rescue_cache().await?;
-//     Ok(())
-// }
-
 pub fn show_in_folder(path: PathBuf) -> Result<()> {
     {
         #[cfg(target_os = "windows")]
@@ -88,85 +77,11 @@ struct Payload {
     cwd: String,
 }
 
-#[tokio::main]
-// if Tauri app is called with arguments, then those arguments will be treated as commands
-// ie: deep links or filepaths for .mrpacks
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         let path = &args[1];
-        // let status = Command::new("cp")
-        // .arg(first_arg)
-        // .arg("/root/.config/com.modrinth.theseus/profiles/test/mods/test.jar")
-        // .status();
-
-        let profiles = async_io::block_on(async {
-            profile::big_update(path).await?;
-            Ok::<(), theseus::Error>(())
-        });
-
+        profile::big_update(path).await;
     }
-
-    // let profiles = async_io::block_on(async {
-    //     profile::big_update().await?;
-    //     Ok::<(), theseus::Error>(())
-    // });
-
-
-    // let profiles = async_io::block_on(async {
-    //     let res = profile::list(Some(false)).await?;
-    //     Ok::<HashMap<ProfilePathId, Profile>, theseus::Error>(res)
-    // }).unwrap();
-
-    // // ProfilePathId can do .path on one of these
-
-    // // uuid,
-    // // install_stage: ProfileInstallStage::NotInstalled,
-    // // path: PathBuf::new().join(&name),
-    // // metadata: ProfileMetadata {
-    // //     name,
-    // //     icon: None,
-    // //     icon_url: None,
-    // //     groups: vec![],
-    // //     game_version: version,
-    // //     loader: ModLoader::Vanilla,
-    // //     loader_version: None,
-    // //     linked_data: None,
-    // //     date_created: Utc::now(),
-    // //     date_modified: Utc::now(),
-    // //     last_played: None,
-    // //     submitted_time_played: 0,
-    // //     recent_time_played: 0,
-    // // },
-    // // projects: HashMap::new(),
-    // // java: None,
-    // // memory: None,
-    // // resolution: None,
-    // // fullscreen: None,
-    // // hooks: None,
-    // // modrinth_update_version: None,
-
-
-    // // pub struct Project {
-    // //     pub sha512: String,
-    // //     pub disabled: bool,
-    // //     pub metadata: ProjectMetadata,
-    // //     pub file_name: String,
-    // // }
-
-
-    // let profiles = async_io::block_on(async {
-    //     for (instance_path, value) in &profiles {
-    //         let projects = &value.projects;
-    //         for (project_path, project) in projects {
-    //             //profile::update_project(&instance_path, &project_path, None).await?;
-    //             let fullPath = profile::get_mod_full_path(&instance_path, &project_path).await.unwrap();
-    //             show_in_folder(fullPath);
-    //             profile::toggle_disable_project(&instance_path, &project_path).await?;
-    //             profile::toggle_disable_project(&instance_path, &project_path).await?;
-    //             profile::remove_project(&instance_path, &project_path).await?;
-    //         }
-    //     }
-    //     Ok::<(), theseus::Error>(())
-    // });
 }
